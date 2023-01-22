0e095d379800b024a6989597ae35914fc020a5bf
Tyler Coats <coatsctyler@gmail.com>
2023-01-22 16:59:35 -0500

    added global variables and two classes

---
+++ app.js
@@ -0,0 +1,42 @@
+const playingCards = []
+const cardSuites = ["diamonds", "spades", "clubs", "hearts" ]
+const cardValues = []
+const player = "";
+const dealer = "";
+const houseMoney = 0;
+const playerMoney = 0;
+const wager = 0;
+
+class Player {
+    constructor(name) {
+        this.name = name
+        this.cash = cash
+        this.winAmount = winAmount
+    }
+    wager(cash) {
+
+    }
+}
+
+class Dealer {
+    constructor() {
+        this.cash = cash
+        this.winAmount = winAmount
+    }
+    deal() {
+
+    }
+
+}
+
+const askName = () => {
+    let = playerName = prompt("What's your name?")
+    return playerName
+}
+
+askName()
+
+console.log(playerName);
+
+
+
